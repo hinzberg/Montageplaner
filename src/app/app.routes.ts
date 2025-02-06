303400515a0a80e1246c9d7077eb32fab938c011
LNAG\holgerh <holgerh@amana.lucanet.com>
2025-02-06 15:56:59 +0100

    2025-02-06 Project created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SplashComponent } from './Components/splash/splash.component';
+import { SettingsComponent } from './Components/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // The path with an empty name is the default page and will be loaded automatically at startup.
+  { path: '', component: SplashComponent },
+  { path: 'settings', component: SettingsComponent },
+];
