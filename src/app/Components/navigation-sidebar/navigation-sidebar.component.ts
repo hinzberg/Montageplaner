c2bd2d6df73b95befd592ee9ca9fbf6aaa0de0c1
LNAG\holgerh <holgerh@amana.lucanet.com>
2025-05-02 15:19:26 +0200

    2025-05-02 Basic Routing

--- src/app/Components/navigation-sidebar/navigation-sidebar.component.ts
+++ src/app/Components/navigation-sidebar/navigation-sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navigation-sidebar.component.html',
-  styleUrl: './navigation-sidebar.component.scss'
+  styleUrl: './navigation-sidebar.component.scss',
 })
 export class NavigationSidebarComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { WrongRouteComponent} from "./Components/wrong-route/wrong-route.compone
 export const routes: Routes = [
   // The path with an empty name is the default page and will be loaded automatically at startup.
   { path: '', component: SplashComponent },
+  { path: 'splash', component: SplashComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: '**', pathMatch: 'full',component: WrongRouteComponent}
+ // { path: '**', pathMatch: 'full',component: WrongRouteComponent}
 ];
