3d890919eb3ecd690233b466236a62591bb79ac1
LNAG\holgerh <holgerh@amana.lucanet.com>
2025-05-13 15:56:21 +0200

    2025-05-13 Added Local Storage for Personnel Service

--- src/app/core/services/personnel.service.ts
+++ src/app/core/services/personnel.service.ts
@@ -2,14 +2,24 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Person } from '../models/person.model';
 import { Profession } from '../models/profession.enum';
 
+const STORAGE_KEY = 'montageplaner_persons';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PersonnelService {
+  
+  // This is actual the list of persons
   private persons: Person[] = [];
+
+  // This is the event emitter for the persons list
+  //  It's used to notify components that are subscribed to it about changes in the personnel list.
   public personsUpdated = new EventEmitter<Person[]>();
 
-  constructor() { }
+  constructor() {
+    // Load persons from local storage on service initialization
+    this.loadPersonsFromStorage();
+  }
 
   // Get all persons
   getPersons(): Person[] {
@@ -20,6 +30,7 @@ export class PersonnelService {
   addPerson(person: Person): void {
     this.persons.push(person);
     this.personsUpdated.emit([...this.persons]);
+    this.savePersonsToStorage();
   }
 
   // Remove a person by id
@@ -28,6 +39,7 @@ export class PersonnelService {
     if (index !== -1) {
       this.persons.splice(index, 1);
       this.personsUpdated.emit([...this.persons]);
+      this.savePersonsToStorage();
     }
   }
 
@@ -37,6 +49,7 @@ export class PersonnelService {
     if (index !== -1) {
       this.persons[index] = updatedPerson;
       this.personsUpdated.emit([...this.persons]);
+      this.savePersonsToStorage();
     }
   }
 
@@ -49,10 +62,34 @@ export class PersonnelService {
   clearPersons(): void {
     this.persons = [];
     this.personsUpdated.emit([]);
+    this.savePersonsToStorage();
   }
 
   // Get current number of persons
   getPersonCount(): number {
     return this.persons.length;
+  }
+
+  private loadPersonsFromStorage(): void {
+    const storedPersons = localStorage.getItem(STORAGE_KEY);
+    if (storedPersons) {
+      try {
+        // Parse the stored JSON and recreate Person objects
+        const parsedPersons = JSON.parse(storedPersons);
+        this.persons = parsedPersons.map((p: any) => new Person(p.firstName, p.lastName, p.profession));
+        this.personsUpdated.emit([...this.persons]);
+      } catch (error) {
+        console.error('Error loading persons from storage:', error);
+        this.persons = [];
+      }
+    }
+  }
+
+  private savePersonsToStorage(): void {
+    try {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.persons));
+    } catch (error) {
+      console.error('Error saving persons to storage:', error);
+    }
   }
 } 
