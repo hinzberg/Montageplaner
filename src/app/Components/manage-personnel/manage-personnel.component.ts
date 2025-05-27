f032326ec940ac8931eb4043fe64dbdb99ac5a27
LNAG\holgerh <holgerh@amana.lucanet.com>
2025-05-27 15:24:44 +0200

    2025-05-27 Boilerplate for Manage-Personel

--- src/app/Components/manage-personnel/manage-personnel.component.ts
+++ src/app/Components/manage-personnel/manage-personnel.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PersonnelService } from '../../core/services/personnel.service';
+import { Person } from '../../core/models/person.model';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-manage-personnel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage-personnel.component.html',
   styleUrl: './manage-personnel.component.scss'
 })
-export class ManagePersonnelComponent {
+export class ManagePersonnelComponent implements OnInit, OnDestroy {
+  persons: Person[] = [];
+  private subscription: Subscription = new Subscription();
 
+  constructor(private personnelService: PersonnelService) {}
+
+  ngOnInit(): void {
+    // Get initial list of persons
+    this.persons = this.personnelService.getPersons();
+
+    // Subscribe to updates
+    this.subscription.add(
+      this.personnelService.personsUpdated.subscribe(persons => {
+        this.persons = persons;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    // Clean up subscription when component is destroyed
+    this.subscription.unsubscribe();
+  }
 }
