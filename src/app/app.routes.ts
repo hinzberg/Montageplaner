c6c5a260080ac63b5dabb8c3341ba3f6bf911409
Holger Hinzberg <holger@hinzberg.de>
2025-10-05 14:46:12 +0200

    2025-10-05 Added Playground Sample: Dialog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,14 @@ import { ManageEquipmentComponent } from "./Components/manage-equipment/manage-e
 import { PlanAssemblyComponent} from "./Components/plan-assembly/plan-assembly.component";
 import { MaintenanceComponent} from "./Components/maintenance/maintenance.component";
 import { ListParentComponent } from "./playground/sample-list-child/list-parent/list-parent.component";
+import { DialogParentComponent} from "./playground/sample-dialog-open/dialog-parent/dialog-parent.component";
+import { MessageboxComponent} from "./Components/messagebox/messagebox.component";
 
 export const routes: Routes = [
+  // { path: '', component: MessageboxComponent },
   // The path with an empty name is the default page and will be loaded automatically at startup.
   { path: '', component: SplashComponent },
+  //{ path: '', component: DialogParentComponent },
   // { path: '', component: ListParentComponent }, // Playground Sample list-child
   { path: 'splash', component: SplashComponent },
   { path: 'addPeople', component: AddPeopleComponent },
