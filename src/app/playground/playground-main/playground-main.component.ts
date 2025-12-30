843ac5290bf073b4f6930a4d9a9697e8223667bc
Holger Hinzberg <holger@hinzberg.de>
2025-12-30 15:48:26 +0100

    2025-12-30  Used loop for Playground Sample navigation

--- src/app/playground/playground-main/playground-main.component.ts
+++ src/app/playground/playground-main/playground-main.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import {RouterLinkActive} from "@angular/router";
+import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-playground-main',
   standalone: true,
   imports: [
-    RouterLinkActive, RouterModule
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './playground-main.component.html',
   styleUrl: './playground-main.component.scss'
 })
 export class PlaygroundMainComponent {
-
+  playgroundLinks = [
+    { link: '/playground-two-way', description: 'Two-Way-Binding' },
+    { link: '/playground-pipes', description: 'Build-In Pipes' },
+    { link: '/playground-custom-pipes', description: 'Custom Pipes' },
+    { link: '/playground-class-binding', description: 'Class Binding' },
+    { link: '/playground-style-binding', description: 'Style Binding' },
+    { link: '/playground-array', description: 'Array' },
+    { link: '/playground-simple-signal', description: 'Simple Signal' },
+    { link: '/playground-event-binding', description: 'Event Binding' },
+    { link: '/playground-country-service', description: 'Service' },
+    { link: '/playground-list-parent', description: 'Simple List' },
+    { link: '/playground-dialog-parent', description: 'Dialog Window Demo' }
+  ];
 }
