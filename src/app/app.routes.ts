54cb84c876710b1fd85dcb9707b3f1735cf7c9ac
Holger Hinzberg <holger@hinzberg.de>
2025-12-30 15:36:02 +0100

    2025-12-30  Added Playground Samples

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,19 +12,16 @@ import { PlaygroundMainComponent} from "./playground/playground-main/playground-
 
 // Playground imports
 import { TwowayBindingComponent} from "./playground/twoway-binding/twoway-binding.component";
-/*
-import { ListParentComponent } from "./playground/sample-list-child/list-parent/list-parent.component";
-import { DialogParentComponent} from "./playground/sample-dialog-open/dialog-parent/dialog-parent.component";
-import {SimpleSignalComponent} from "./playground/simple-signal/simple-signal.component";
-import { ClassBindingComponent} from "./playground/class-binding/class-binding.component";
-import { StyleBindingComponent } from "./playground/style-binding/style-binding.component";
-import { EventBindingComponent} from "./playground/event-binding/event-binding.component";
-import { ArraySampleComponent} from "./playground/array-sample/array-sample.component";
 import { SamplePipesComponent } from './playground/sample-pipes/sample-pipes.component';
 import { SampleCustomPipeComponent} from "./playground/sample-custom-pipe/sample-custom-pipe.component";
-*/
-
+import { ClassBindingComponent} from "./playground/class-binding/class-binding.component";
+import { StyleBindingComponent } from "./playground/style-binding/style-binding.component";
+import { ArraySampleComponent} from "./playground/array-sample/array-sample.component";
+import {SimpleSignalComponent} from "./playground/simple-signal/simple-signal.component";
+import { EventBindingComponent} from "./playground/event-binding/event-binding.component";
 import { DataserviceSampleComponent} from "./playground/dataservice-sample/dataservice-sample.component";
+import { ListParentComponent } from "./playground/sample-list-child/list-parent/list-parent.component";
+import { DialogParentComponent} from "./playground/sample-dialog-open/dialog-parent/dialog-parent.component";
 
 export const routes: Routes = [
   // The path with an empty name is the default page and will be loaded automatically at startup.
@@ -38,6 +35,16 @@ export const routes: Routes = [
   { path: 'settings', component: SettingsComponent },
   { path: 'maintenance', component: MaintenanceComponent },
   { path: 'playground', component: PlaygroundMainComponent },
+  { path: 'playground-class-binding', component: ClassBindingComponent },
+  { path: 'playground-style-binding', component: StyleBindingComponent },
+  { path: 'playground-array', component: ArraySampleComponent },
   { path: 'playground-two-way', component: TwowayBindingComponent },
+  { path: 'playground-pipes', component: SamplePipesComponent },
+  { path: 'playground-custom-pipes', component: SampleCustomPipeComponent },
+  { path: 'playground-simple-signal', component: SimpleSignalComponent },
+  { path: 'playground-event-binding', component: EventBindingComponent },
+  { path: 'playground-country-service', component: DataserviceSampleComponent },
+  { path: 'playground-list-parent', component: ListParentComponent },
+  { path: 'playground-dialog-parent', component: DialogParentComponent },
   { path: '**', pathMatch: 'full',component: WrongRouteComponent}
 ];
